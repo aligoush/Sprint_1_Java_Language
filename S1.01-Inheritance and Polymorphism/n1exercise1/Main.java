49e82359f95800c4adcb12481b1ce5093c7cb66b
alinaltyn <alieneguzh@gmail.com>
2024-09-18 11:11:44 +0200

    correct exercise 1

--- S1.01-Inheritance and Polymorphism/n1exercise1/Main.java
+++ S1.01-Inheritance and Polymorphism/n1exercise1/Main.java
@@ -2,7 +2,6 @@ package n1exercise1;
 
 public class Main {
     public static void main(java.lang.String[] args) {
-
         Instrument flute = new WindInstrument("Flute", 300);
         Instrument violin = new StringInstrument("Violin", 700);
         Instrument drum = new PercussionInstrument("Drum", 101.50);
